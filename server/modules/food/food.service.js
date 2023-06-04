b4402c76f30ab93878470310fd07146ab7007c4e
VietDung7301 <vietdung.7301@gmail.com>
2023-06-04 23:00:00 +0700

    no message

--- server/modules/food/food.service.js
+++ server/modules/food/food.service.js
@@ -1,28 +1,31 @@
 const sql = require("../../seed/queryMysqlDB");
 
 exports.getFoodDescriptionList = async (keyword, page=1, pageSize=10) => {
-    const keySearch = ''
-    if (keyword) keySearch = ``
+    let keySearch = ''
+    if (keyword) keySearch = `AND food.name like '%${keyword}%'`
 
-    const paging = `LIMIT ${((page - 1) * pageSize) + 1}, ${page * pageSize}`
+    let paging = `LIMIT ${((page - 1) * pageSize)}, ${pageSize}`
 
-    query = `SELECT image.Src as img, AVG(rating) AS rating, food.name, price, fooddesciption.Desciption as description, restaurant.name as restaurantName, restaurant.OpenTime, restaurant.CloseTime, restaurant.Address
-        FROM food, fooddesciption, restaurant, image, FoodReview
-        WHERE food.id = fooddesciption.FoodID
-        AND fooddesciption.id = foodreview.FoodDesId
-        AND fooddesciption.GroupImageId = image.GroupId
-        AND fooddesciption.RestaurantID = restaurant.ID
+    query = `SELECT fooddescription.id, image.Src as img, AVG(rating) AS rating, food.name, price, fooddescription.Description as description, restaurant.name as restaurantName, restaurant.id AS restaurantId, restaurant.OpenTime, restaurant.CloseTime, restaurant.Address
+        FROM fooddescription
+        JOIN food on food.id = fooddescription.FoodID
+        LEFT JOIN FoodReview on fooddescription.id = foodreview.FoodDesId
+        JOIN restaurant on fooddescription.RestaurantID = restaurant.ID
+        JOIN image on fooddescription.GroupImageId = image.GroupId
         ${keySearch}
-        GROUP BY description, img, food.name, price, restaurantName, restaurant.OpenTime, restaurant.CloseTime, restaurant.Address
+        GROUP BY fooddescription.id, img
         ${paging}`
+        
     result = await sql.QueryGetData(query)
     for (food of result) {
         food['restaurant'] = {
+            'id': food['restaurantId'],
             'name': food['restaurantName'],
             'openTime': food['OpenTime'],
             'closeTime': food['CloseTime'],
             'address': food['Address']
         }
+        delete food['restaurantId']
         delete food['restaurantName']
         delete food['OpenTime']
         delete food['CloseTime']

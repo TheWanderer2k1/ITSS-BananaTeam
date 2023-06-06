4476cc51ffb7c4b1fdd47b8883840575b905acc0
Lê Đức Huy <leduchuy1461@gmail.com>
2023-06-07 01:26:05 +0700

    Thêm select tirnhd để lọc

--- client/src/modules/intro/components/search.jsx
+++ client/src/modules/intro/components/search.jsx
@@ -1,4 +1,4 @@
-import { Button, Rate, DatePicker, TimePicker  } from 'antd';
+import { Button, Rate, DatePicker, TimePicker, Select  } from 'antd';
 import FoodList from './food-list'
 import FoodItem from "./food-item";
 import { useLocation } from 'react-router-dom';
@@ -31,6 +31,72 @@ let initialRatingFilter = [
 	{ id: 5, value: 5, checked: true }
 ]
 
+let listCityFilter = [
+    { value: "HA Noi", label: "Thành phố Hà Nội" },
+    { value: "Tỉnh Hà Giang", label: "Tỉnh Hà Giang" },
+    { value: "Tỉnh Cao Bằng", label: "Tỉnh Cao Bằng" },
+    { value: "Tỉnh Bắc Kạn", label: "Tỉnh Bắc Kạn" },
+    { value: "Tỉnh Tuyên Quang", label: "Tỉnh Tuyên Quang" },
+    { value: "Tỉnh Lào Cai", label: "Tỉnh Lào Cai" },
+    { value: "Tỉnh Điện Biên", label: "Tỉnh Điện Biên" },
+    { value: "Tỉnh Lai Châu", label: "Tỉnh Lai Châu" },
+    { value: "Tỉnh Sơn La", label: "Tỉnh Sơn La" },
+    { value: "Tỉnh Yên Bái", label: "Tỉnh Yên Bái" },
+    { value: "Tỉnh Hoà Bình", label: "Tỉnh Hoà Bình" },
+    { value: "Tỉnh Thái Nguyên", label: "Tỉnh Thái Nguyên" },
+    { value: "Tỉnh Lạng Sơn", label: "Tỉnh Lạng Sơn" },
+    { value: "Tỉnh Quảng Ninh", label: "Tỉnh Quảng Ninh" },
+    { value: "Tỉnh Bắc Giang", label: "Tỉnh Bắc Giang" },
+    { value: "Tỉnh Phú Thọ", label: "Tỉnh Phú Thọ" },
+    { value: "Tỉnh Vĩnh Phúc", label: "Tỉnh Vĩnh Phúc" },
+    { value: "Tỉnh Bắc Ninh", label: "Tỉnh Bắc Ninh" },
+    { value: "Tỉnh Hải Dương", label: "Tỉnh Hải Dương" },
+    { value: "Hai Phong", label: "Thành phố Hải Phòng" },
+    { value: "Tỉnh Hưng Yên", label: "Tỉnh Hưng Yên" },
+    { value: "Tỉnh Thái Bình", label: "Tỉnh Thái Bình" },
+    { value: "Tỉnh Hà Nam", label: "Tỉnh Hà Nam" },
+    { value: "Tỉnh Nam Định", label: "Tỉnh Nam Định" },
+    { value: "Tỉnh Ninh Bình", label: "Tỉnh Ninh Bình" },
+    { value: "Tỉnh Thanh Hóa", label: "Tỉnh Thanh Hóa" },
+    { value: "Tỉnh Nghệ An", label: "Tỉnh Nghệ An" },
+    { value: "Tỉnh Hà Tĩnh", label: "Tỉnh Hà Tĩnh" },
+    { value: "Tỉnh Quảng Bình", label: "Tỉnh Quảng Bình" },
+    { value: "Tỉnh Quảng Trị", label: "Tỉnh Quảng Trị" },
+    { value: "Tỉnh Thừa Thiên Huế", label: "Tỉnh Thừa Thiên Huế" },
+    { value: "Da Nang", label: "Thành phố Đà Nẵng" },
+    { value: "Tỉnh Quảng Nam", label: "Tỉnh Quảng Nam" },
+    { value: "Tỉnh Quảng Ngãi", label: "Tỉnh Quảng Ngãi" },
+    { value: "Tỉnh Bình Định", label: "Tỉnh Bình Định" },
+    { value: "Tỉnh Phú Yên", label: "Tỉnh Phú Yên" },
+    { value: "Tỉnh Khánh Hòa", label: "Tỉnh Khánh Hòa" },
+    { value: "Tỉnh Ninh Thuận", label: "Tỉnh Ninh Thuận" },
+    { value: "Tỉnh Bình Thuận", label: "Tỉnh Bình Thuận" },
+    { value: "Tỉnh Kon Tum", label: "Tỉnh Kon Tum" },
+    { value: "Tỉnh Gia Lai", label: "Tỉnh Gia Lai" },
+    { value: "Tỉnh Đắk Lắk", label: "Tỉnh Đắk Lắk" },
+    { value: "Tỉnh Đắk Nông", label: "Tỉnh Đắk Nông" },
+    { value: "Tỉnh Lâm Đồng", label: "Tỉnh Lâm Đồng" },
+    { value: "Tỉnh Bình Phước", label: "Tỉnh Bình Phước" },
+    { value: "Tỉnh Tây Ninh", label: "Tỉnh Tây Ninh" },
+    { value: "Tỉnh Bình Dương", label: "Tỉnh Bình Dương" },
+    { value: "Tỉnh Đồng Nai", label: "Tỉnh Đồng Nai" },
+    { value: "Tỉnh Bà Rịa - Vũng Tàu", label: "Tỉnh Bà Rịa - Vũng Tàu" },
+    { value: "TP HCM", label: "Thành phố Hồ Chí Minh" },
+    { value: "Tỉnh Long An", label: "Tỉnh Long An" },
+    { value: "Tỉnh Tiền Giang", label: "Tỉnh Tiền Giang" },
+    { value: "Tỉnh Bến Tre", label: "Tỉnh Bến Tre" },
+    { value: "Tỉnh Trà Vinh", label: "Tỉnh Trà Vinh" },
+    { value: "Tỉnh Vĩnh Long", label: "Tỉnh Vĩnh Long" },
+    { value: "Tỉnh Đồng Tháp", label: "Tỉnh Đồng Tháp" },
+    { value: "Tỉnh An Giang", label: "Tỉnh An Giang" },
+    { value: "Tỉnh Kiên Giang", label: "Tỉnh Kiên Giang" },
+    { value: "Thành phố Cần Thơ", label: "Thành phố Cần Thơ" },
+    { value: "Tỉnh Hậu Giang", label: "Tỉnh Hậu Giang" },
+    { value: "Tỉnh Sóc Trăng", label: "Tỉnh Sóc Trăng" },
+    { value: "Tỉnh Bạc Liêu", label: "Tỉnh Bạc Liêu" },
+    { value: "Tỉnh Cà Mau", label: "Tỉnh Cà Mau" },
+]
+
 function Search () {
     const location = useLocation();
     const [inputValue, setInputValue] = useState('');
@@ -51,6 +117,10 @@ function Search () {
         fetchfoodDecription();
       }
     }
+
+    const handleChangeCityFilter = (value) => {
+        console.log(`selected ${value}`);
+      };
     useEffect(() => {
         const params = new URLSearchParams(location.search);
         const inputValueParam = params.get('search_query');
@@ -201,17 +271,23 @@ function Search () {
                             場所
                         </p>
                         <div id="cityFilter" class="collapse in">
-                            {priceFilter.map((price) => (
+                            {/* {listCityFilter.map((city) => (
                                 <p className="rate-star-item" key={price.id}>
                                 <input type="checkbox" 
                                         onChange={() => {onChangePriceFilter(price.id)}} 
                                         defaultChecked={price.checked}/>
                                     {price.from}-{price.to} VNĐ
                                 </p>
-                            ))}
+                            ))} */}
+                            <Select
+                                allowClear
+                                onChange={handleChangeCityFilter}
+                                options={listCityFilter}
+                                style={{ width: '100%' }}
+                            />
                         </div>
                     </div>
-                    <div className="action-button d-flex">
+                    <div className="action-button d-flex mt-12">
                         <button className="cancel-button">クリア</button>
                         <button className="confirm-button" onClick={handleFilter}>適用</button>
                     </div>
@@ -241,9 +317,9 @@ function Search () {
                     <input type="text" placeholder='Nem cuốn' value={searchData} onChange={handleSearchData} onKeyUp={handleKeyUp}/>
                 </div>
             </div>
-            <Button type="primary" onClick={clickFilter}>
+            {/* <Button type="primary" onClick={clickFilter}>
                 Open the notification box
-            </Button>
+            </Button> */}
         </div>                            
       </div>    
     <div className="food-list d-flex">

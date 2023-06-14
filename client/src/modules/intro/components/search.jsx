f03bb237e70d3d16a04f0034c874392b0d49dcc8
Lê Đức Huy <leduchuy1461@gmail.com>
2023-06-14 16:30:44 +0700

    Thêm select tỉnh thành và quận huyện trong bảng filter

--- client/src/modules/intro/components/search.jsx
+++ client/src/modules/intro/components/search.jsx
@@ -39,69 +39,69 @@ let listSearchOption = [
 ]
 
 let listCityFilter = [
-    { value: "HA Noi", label: "Thành phố Hà Nội" },
-    { value: "Tỉnh Hà Giang", label: "Tỉnh Hà Giang" },
-    { value: "Tỉnh Cao Bằng", label: "Tỉnh Cao Bằng" },
-    { value: "Tỉnh Bắc Kạn", label: "Tỉnh Bắc Kạn" },
-    { value: "Tỉnh Tuyên Quang", label: "Tỉnh Tuyên Quang" },
-    { value: "Tỉnh Lào Cai", label: "Tỉnh Lào Cai" },
-    { value: "Tỉnh Điện Biên", label: "Tỉnh Điện Biên" },
-    { value: "Tỉnh Lai Châu", label: "Tỉnh Lai Châu" },
-    { value: "Tỉnh Sơn La", label: "Tỉnh Sơn La" },
-    { value: "Tỉnh Yên Bái", label: "Tỉnh Yên Bái" },
-    { value: "Tỉnh Hoà Bình", label: "Tỉnh Hoà Bình" },
-    { value: "Tỉnh Thái Nguyên", label: "Tỉnh Thái Nguyên" },
-    { value: "Tỉnh Lạng Sơn", label: "Tỉnh Lạng Sơn" },
-    { value: "Tỉnh Quảng Ninh", label: "Tỉnh Quảng Ninh" },
-    { value: "Tỉnh Bắc Giang", label: "Tỉnh Bắc Giang" },
-    { value: "Tỉnh Phú Thọ", label: "Tỉnh Phú Thọ" },
-    { value: "Tỉnh Vĩnh Phúc", label: "Tỉnh Vĩnh Phúc" },
-    { value: "Tỉnh Bắc Ninh", label: "Tỉnh Bắc Ninh" },
-    { value: "Tỉnh Hải Dương", label: "Tỉnh Hải Dương" },
-    { value: "Hai Phong", label: "Thành phố Hải Phòng" },
-    { value: "Tỉnh Hưng Yên", label: "Tỉnh Hưng Yên" },
-    { value: "Tỉnh Thái Bình", label: "Tỉnh Thái Bình" },
-    { value: "Tỉnh Hà Nam", label: "Tỉnh Hà Nam" },
-    { value: "Tỉnh Nam Định", label: "Tỉnh Nam Định" },
-    { value: "Tỉnh Ninh Bình", label: "Tỉnh Ninh Bình" },
-    { value: "Tỉnh Thanh Hóa", label: "Tỉnh Thanh Hóa" },
-    { value: "Tỉnh Nghệ An", label: "Tỉnh Nghệ An" },
-    { value: "Tỉnh Hà Tĩnh", label: "Tỉnh Hà Tĩnh" },
-    { value: "Tỉnh Quảng Bình", label: "Tỉnh Quảng Bình" },
-    { value: "Tỉnh Quảng Trị", label: "Tỉnh Quảng Trị" },
-    { value: "Tỉnh Thừa Thiên Huế", label: "Tỉnh Thừa Thiên Huế" },
-    { value: "Da Nang", label: "Thành phố Đà Nẵng" },
-    { value: "Tỉnh Quảng Nam", label: "Tỉnh Quảng Nam" },
-    { value: "Tỉnh Quảng Ngãi", label: "Tỉnh Quảng Ngãi" },
-    { value: "Tỉnh Bình Định", label: "Tỉnh Bình Định" },
-    { value: "Tỉnh Phú Yên", label: "Tỉnh Phú Yên" },
-    { value: "Tỉnh Khánh Hòa", label: "Tỉnh Khánh Hòa" },
-    { value: "Tỉnh Ninh Thuận", label: "Tỉnh Ninh Thuận" },
-    { value: "Tỉnh Bình Thuận", label: "Tỉnh Bình Thuận" },
-    { value: "Tỉnh Kon Tum", label: "Tỉnh Kon Tum" },
-    { value: "Tỉnh Gia Lai", label: "Tỉnh Gia Lai" },
-    { value: "Tỉnh Đắk Lắk", label: "Tỉnh Đắk Lắk" },
-    { value: "Tỉnh Đắk Nông", label: "Tỉnh Đắk Nông" },
-    { value: "Tỉnh Lâm Đồng", label: "Tỉnh Lâm Đồng" },
-    { value: "Tỉnh Bình Phước", label: "Tỉnh Bình Phước" },
-    { value: "Tỉnh Tây Ninh", label: "Tỉnh Tây Ninh" },
-    { value: "Tỉnh Bình Dương", label: "Tỉnh Bình Dương" },
-    { value: "Tỉnh Đồng Nai", label: "Tỉnh Đồng Nai" },
-    { value: "Tỉnh Bà Rịa - Vũng Tàu", label: "Tỉnh Bà Rịa - Vũng Tàu" },
-    { value: "TP HCM", label: "Thành phố Hồ Chí Minh" },
-    { value: "Tỉnh Long An", label: "Tỉnh Long An" },
-    { value: "Tỉnh Tiền Giang", label: "Tỉnh Tiền Giang" },
-    { value: "Tỉnh Bến Tre", label: "Tỉnh Bến Tre" },
-    { value: "Tỉnh Trà Vinh", label: "Tỉnh Trà Vinh" },
-    { value: "Tỉnh Vĩnh Long", label: "Tỉnh Vĩnh Long" },
-    { value: "Tỉnh Đồng Tháp", label: "Tỉnh Đồng Tháp" },
-    { value: "Tỉnh An Giang", label: "Tỉnh An Giang" },
-    { value: "Tỉnh Kiên Giang", label: "Tỉnh Kiên Giang" },
-    { value: "Thành phố Cần Thơ", label: "Thành phố Cần Thơ" },
-    { value: "Tỉnh Hậu Giang", label: "Tỉnh Hậu Giang" },
-    { value: "Tỉnh Sóc Trăng", label: "Tỉnh Sóc Trăng" },
-    { value: "Tỉnh Bạc Liêu", label: "Tỉnh Bạc Liêu" },
-    { value: "Tỉnh Cà Mau", label: "Tỉnh Cà Mau" },
+    { value: "HA Noi", label: "Thành phố Hà Nội", id: "01" },
+    { value: "Tỉnh Hà Giang", label: "Tỉnh Hà Giang", id: "02" },
+    { value: "Tỉnh Cao Bằng", label: "Tỉnh Cao Bằng", id: "04" },
+    { value: "Tỉnh Bắc Kạn", label: "Tỉnh Bắc Kạn", id: "06" },
+    { value: "Tỉnh Tuyên Quang", label: "Tỉnh Tuyên Quang", id: "08" },
+    { value: "Tỉnh Lào Cai", label: "Tỉnh Lào Cai", id: "10" },
+    { value: "Tỉnh Điện Biên", label: "Tỉnh Điện Biên", id: "11" },
+    { value: "Tỉnh Lai Châu", label: "Tỉnh Lai Châu", id: "12" },
+    { value: "Tỉnh Sơn La", label: "Tỉnh Sơn La", id: "14" },
+    { value: "Tỉnh Yên Bái", label: "Tỉnh Yên Bái", id: "15" },
+    { value: "Tỉnh Hoà Bình", label: "Tỉnh Hoà Bình", id: "17" },
+    { value: "Tỉnh Thái Nguyên", label: "Tỉnh Thái Nguyên", id: "19" },
+    { value: "Tỉnh Lạng Sơn", label: "Tỉnh Lạng Sơn", id: "20" },
+    { value: "Tỉnh Quảng Ninh", label: "Tỉnh Quảng Ninh", id: "22" },
+    { value: "Tỉnh Bắc Giang", label: "Tỉnh Bắc Giang", id: "24" },
+    { value: "Tỉnh Phú Thọ", label: "Tỉnh Phú Thọ", id: "25" },
+    { value: "Tỉnh Vĩnh Phúc", label: "Tỉnh Vĩnh Phúc", id: "26" },
+    { value: "Tỉnh Bắc Ninh", label: "Tỉnh Bắc Ninh", id: "27" },
+    { value: "Tỉnh Hải Dương", label: "Tỉnh Hải Dương", id: "30" },
+    { value: "Hai Phong", label: "Thành phố Hải Phòng", id: "31" },
+    { value: "Tỉnh Hưng Yên", label: "Tỉnh Hưng Yên", id: "33" },
+    { value: "Tỉnh Thái Bình", label: "Tỉnh Thái Bình", id: "34" },
+    { value: "Tỉnh Hà Nam", label: "Tỉnh Hà Nam", id: "35" },
+    { value: "Tỉnh Nam Định", label: "Tỉnh Nam Định", id: "36" },
+    { value: "Tỉnh Ninh Bình", label: "Tỉnh Ninh Bình", id: "37" },
+    { value: "Tỉnh Thanh Hóa", label: "Tỉnh Thanh Hóa", id: "38" },
+    { value: "Tỉnh Nghệ An", label: "Tỉnh Nghệ An", id: "40" },
+    { value: "Tỉnh Hà Tĩnh", label: "Tỉnh Hà Tĩnh", id: "42" },
+    { value: "Tỉnh Quảng Bình", label: "Tỉnh Quảng Bình", id: "44" },
+    { value: "Tỉnh Quảng Trị", label: "Tỉnh Quảng Trị", id: "45" },
+    { value: "Tỉnh Thừa Thiên Huế", label: "Tỉnh Thừa Thiên Huế", id: "46" },
+    { value: "Da Nang", label: "Thành phố Đà Nẵng", id: "48" },
+    { value: "Tỉnh Quảng Nam", label: "Tỉnh Quảng Nam", id: "49" },
+    { value: "Tỉnh Quảng Ngãi", label: "Tỉnh Quảng Ngãi", id: "51" },
+    { value: "Tỉnh Bình Định", label: "Tỉnh Bình Định", id: "52" },
+    { value: "Tỉnh Phú Yên", label: "Tỉnh Phú Yên", id: "54" },
+    { value: "Tỉnh Khánh Hòa", label: "Tỉnh Khánh Hòa", id: "56" },
+    { value: "Tỉnh Ninh Thuận", label: "Tỉnh Ninh Thuận", id: "58" },
+    { value: "Tỉnh Bình Thuận", label: "Tỉnh Bình Thuận", id: "60" },
+    { value: "Tỉnh Kon Tum", label: "Tỉnh Kon Tum", id: "62" },
+    { value: "Tỉnh Gia Lai", label: "Tỉnh Gia Lai", id: "64" },
+    { value: "Tỉnh Đắk Lắk", label: "Tỉnh Đắk Lắk", id: "66" },
+    { value: "Tỉnh Đắk Nông", label: "Tỉnh Đắk Nông", id: "67" },
+    { value: "Tỉnh Lâm Đồng", label: "Tỉnh Lâm Đồng", id: "68" },
+    { value: "Tỉnh Bình Phước", label: "Tỉnh Bình Phước", id: "70" },
+    { value: "Tỉnh Tây Ninh", label: "Tỉnh Tây Ninh", id: "72" },
+    { value: "Tỉnh Bình Dương", label: "Tỉnh Bình Dương", id: "74" },
+    { value: "Tỉnh Đồng Nai", label: "Tỉnh Đồng Nai", id: "75" },
+    { value: "Tỉnh Bà Rịa - Vũng Tàu", label: "Tỉnh Bà Rịa - Vũng Tàu", id: "77" },
+    { value: "TP HCM", label: "Thành phố Hồ Chí Minh", id: "79" },
+    { value: "Tỉnh Long An", label: "Tỉnh Long An", id: "80" },
+    { value: "Tỉnh Tiền Giang", label: "Tỉnh Tiền Giang", id: "82" },
+    { value: "Tỉnh Bến Tre", label: "Tỉnh Bến Tre", id: "83" },
+    { value: "Tỉnh Trà Vinh", label: "Tỉnh Trà Vinh", id: "84" },
+    { value: "Tỉnh Vĩnh Long", label: "Tỉnh Vĩnh Long", id: "86" },
+    { value: "Tỉnh Đồng Tháp", label: "Tỉnh Đồng Tháp", id: "87" },
+    { value: "Tỉnh An Giang", label: "Tỉnh An Giang", id: "89" },
+    { value: "Tỉnh Kiên Giang", label: "Tỉnh Kiên Giang", id: "91" },
+    { value: "Thành phố Cần Thơ", label: "Thành phố Cần Thơ", id: "92" },
+    { value: "Tỉnh Hậu Giang", label: "Tỉnh Hậu Giang", id: "93" },
+    { value: "Tỉnh Sóc Trăng", label: "Tỉnh Sóc Trăng", id: "94" },
+    { value: "Tỉnh Bạc Liêu", label: "Tỉnh Bạc Liêu", id: "95" },
+    { value: "Tỉnh Cà Mau", label: "Tỉnh Cà Mau", id: "96" },
 ]
 
 function Search () {
@@ -117,8 +117,11 @@ function Search () {
 	const [priceFilter, setPriceFilter] = useState(initialPriceFilter);
 	const [ratingFilter, setRatingFilter] = useState(initialRatingFilter);
 	const [cityFilter, setCityFilter] = useState(null);
+	const [districtFilter, setDistrictFilter] = useState(null);
+	const [listDistrictFilter, setListDistrictFilter] = useState([]);
 	const [searchOption, setSearchOption] = useState(listSearchOption[0].value);
     
+
     const moveToHomePage = () => {
         // Navigate to the desired route
         history.push('/homepage');
@@ -137,9 +140,15 @@ function Search () {
     const handleChangeCityFilter = (value) => {
         console.log(`selected ${value}`);
         setCityFilter(value);
-      };
+        fetchDistrict(value);
+    };
 
-      const handleChangeSearchOption = (value) => {
+    const handleChangeDistrictFilter = (value) => {
+        console.log(`selected ${value}`);
+        setDistrictFilter(value);
+    };
+
+    const handleChangeSearchOption = (value) => {
         setSearchOption(value);
       };
     useEffect(() => {
@@ -162,6 +171,40 @@ function Search () {
 			setAllFoodDescription(resp.data['content'])
 			console.log('in fetch', resp.data['content'], foodDecription)
   };
+
+//   const fetchDistrict = async (provinceValue) => {
+//     var provinceId = listCityFilter.find(x => x.value === provinceValue).id;
+//     var url = 'https://vapi.vnappmob.com/api/province/district/' + provinceId;
+//     const resp = await sendRequest({
+//         url: url,
+//         method: "GET",
+//     })
+//     var a = [];
+//     console.log(resp.data['results']);
+//     resp.data['results'].forEach(item => {
+//         a.push({...item, value: item.district_name, label: item.district_name})
+//     })
+//     console.log(a);
+//     setListDistrictFilter(a);
+//     console.log(listDistrictFilter);
+//   }
+
+const fetchDistrict = async (provinceValue) => {
+    var provinceId = listCityFilter.find(x => x.value === provinceValue).id;
+    var url = 'https://vapi.vnappmob.com/api/province/district/' + provinceId;
+    const resp = await sendRequest({
+        url: url,
+        method: "GET",
+    })
+    // var a = [];
+    // resp.data['results'].map(item => ({
+    //     value: item.district_name, label: item.district_name})
+    // );
+    setListDistrictFilter(resp.data['results'].map(item => ({
+        value: item.district_name, label: item.district_name})
+    ));
+    console.log('came here');
+  }
 
   const sortByPriceIncrease = () => {
     setfoodDecription([...foodDecription].sort((a, b) => a.price - b.price));
@@ -340,23 +383,27 @@ function Search () {
                             <i class="fa fa-lock mr-6"></i>
                             場所
                         </p>
-                        <div id="cityFilter" class="collapse">
-                            {/* {listCityFilter.map((city) => (
-                                <p className="rate-star-item" key={price.id}>
-                                <input type="checkbox" 
-                                        onChange={() => {onChangePriceFilter(price.id)}} 
-                                        defaultChecked={price.checked}/>
-                                    {price.from}-{price.to} VNĐ
-                                </p>
-                            ))} */}
-                            <Select
-                                allowClear
-                                placeholder='都道府県'
-                                value={cityFilter}
-                                onChange={handleChangeCityFilter}
-                                options={listCityFilter}
-                                style={{ width: '100%' }}
-                            />
+                        <div id="cityFilter" class="collapse row">                            
+                            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6" >
+                                <Select
+                                    allowClear
+                                    placeholder='市'
+                                    value={cityFilter}
+                                    onChange={handleChangeCityFilter}
+                                    options={listCityFilter}
+                                    style={{ width: '100%' }}
+                                />
+                            </div>   
+                            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6" >
+                                <Select
+                                    allowClear
+                                    placeholder='区'
+                                    value={districtFilter}
+                                    onChange={handleChangeDistrictFilter}
+                                    options={listDistrictFilter}
+                                    style={{ width: '100%' }}
+                                />
+                            </div>
                         </div>
                     </div>
                     <div className="action-button d-flex mt-12">
@@ -383,13 +430,17 @@ function Search () {
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6" >
         </div>
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-2" >
-            <Select
+            {/* <Select
                 placeholder='都道府県'
                 value={searchOption}
                 onChange={handleChangeSearchOption}
                 options={listSearchOption}
                 style={{ width: '100%'}}
-            />
+            /> */}
+            <select class="form-select select-search-option" aria-label="Default select example">
+                <option value="1" selected>料理の名前</option>
+                <option value="2">場所</option>
+            </select>
         </div>
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4" >
             <div className="search-box d-flex">

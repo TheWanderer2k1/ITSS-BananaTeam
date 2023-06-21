1034f24bcc5c713be680ee6890c5d969ed6a13df
Lê Đức Huy <leduchuy1461@gmail.com>
2023-06-21 14:36:04 +0700

    Sửa lại css search screen

--- client/src/modules/intro/components/food-item.jsx
+++ client/src/modules/intro/components/food-item.jsx
@@ -6,14 +6,14 @@ const FoodItem =  ({ image_alt, image_src, name, price, rating, restaurant }) =>
   return (
     <div className="food-item">
         <div className="row d-flex">
-            <div className="food-image">
+            <div className="hfood-image">
                 <img src={image_src} alt={image_alt} />
                 {/* <div className="food-rate">{Math.round(rating)} <i class="fa fa-star"></i></div> */}
                 {/* <div className="food-rate">{rating} <i class="fa fa-star"></i></div> */}
                 <div className="food-rate">{rating ? rating.toFixed(2): Math.round(rating)} <i class="fa fa-star"></i></div>
             </div>
             <div className="food-info">
-                <div className="food-name">{name}</div>
+                <div className="hfood-name">{name}</div>
                 <div className="food-restaurant"><i class="fa fa-pin"></i> {restaurant.name + ', ' + restaurant.address}</div>
                 <div className="food-restaurant-time"><i class="fa fa-pin"></i> {restaurant.openTime + ' - ' + restaurant.closeTime}</div>
                 {/* <div className="food-price"><i class="fa fa-money"></i> {price} 円</div> */}
